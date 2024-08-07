2225722fd7d5b5c6a7ace883e98fb3a3e6c96899
ANMOL\anmol <anmols@hotmail.com>
2024-08-07 13:24:02 +1000

    chore - global config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,23 +13,35 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    colors: {
-      'rivo':{
-        100:'#D3E2D7',
-        200:'#A9D4BA',
-        300:'#93B9A2',
-        400:'#C2EFD4',
-        500:'#6BC785',
-        600:'#267D49',
-        700: '#224F34',
+      maxWidth:{
+        'rivoWidth':'1380px'
       },
-      'rivoText':{
-        100:'#373737',
-        200:'#454545'
+      colors: {
+        'rivo':{
+          100:'#D3E2D7',
+          200:'#A9D4BA',
+          300:'#93B9A2',
+          400:'#C2EFD4',
+          500:'#6BC785',
+          600:'#267D49',
+          700: '#224F34',
+        },
+        'rivoText':{
+          100:'#373737',
+          200:'#454545'
+        }
+  
+      },
+      fontFamily: {
+        poppins: ["var(--font-poppins)"],
+        robotoSlab: ["var(--font-robotoSlab)"],
+        rufina: ["var(--font-rufina)"],
+      },
+      fontSize:{
+        base:'1.375rem'
       }
-
-    }
+    },
+    
   },
   plugins: [],
 };
